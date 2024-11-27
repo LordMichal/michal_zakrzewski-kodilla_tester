f429fd72d917b0259ab1d6c65a04dad65b8fd023
Michal Zakrzewski <michalzakrzewski.mz@gmail.com>
2024-11-27 09:48:15 +0100

    2.8.8.8

--- kodilla-intro/src/main/java/Grades.java
+++ kodilla-intro/src/main/java/Grades.java
@@ -8,27 +8,36 @@ public class Grades {
         this.size = 0;
     }
 
-    public void add() {
+    public double add() {
         if (this.size == 10) {
-            return;
-        }
-        this.grades[this.size] = size;
-        this.size++;
+            return 0;
 
+            this.grades[this.size] = size;
+            this.size++;
+        }
         this.grades = grades;
         for (int i = this.size - 1; i >= 0; i--) {
             if (grades[size] != 0) {
-        System.out.println("Ostatnia dodana ocena:" + grades[size]);
-                break;}
-
-        double Suma = 0;
-            for (double ocena : grades) {
-            Suma += ocena;}
-        double srednia = Suma / grades.length;
-        System.out.println("Średnia ocen: " + srednia);
+                System.out.println("Ostatnia dodana ocena:" + grades[size]);
+                return grades[size] - 1;
             }
+
+            double Suma = 0;
+            for (double ocena : grades) {
+                Suma += ocena;
+                double srednia = Suma / grades.length;
+                return Suma / grades.length;
+                System.out.println("Średnia ocen to: " + srednia);
+            }
+
+
         }
     }
+
+}
+
+
+
 
 
 
