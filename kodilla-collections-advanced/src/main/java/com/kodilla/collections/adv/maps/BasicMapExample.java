1f934cd2138427f6e345bb8cd7b7be5697252f71
Michal Zakrzewski <michalzakrzewski.mz@gmail.com>
2024-11-28 23:08:50 +0100

    1.0

---
+++ kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/maps/BasicMapExample.java
@@ -0,0 +1,10 @@
+package com.kodilla.collections.adv.maps;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class BasicMapExample {
+    public static void main(String[] args) {
+        Map<String, Double> grades = new HashMap<>();
+    }
+}
