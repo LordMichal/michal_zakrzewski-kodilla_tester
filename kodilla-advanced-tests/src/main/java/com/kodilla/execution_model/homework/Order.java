893b6fc3985c6de4a3afa2e8ae531f59973e25e7
Michal Zakrzewski <michalzakrzewski.mz@gmail.com>
2025-01-08 10:47:13 +0100

    7.7

--- kodilla-advanced-tests/src/main/java/com/kodilla/execution_model/homework/Order.java
+++ kodilla-advanced-tests/src/main/java/com/kodilla/execution_model/homework/Order.java
@@ -1,9 +1,35 @@
-//package com.kodilla.execution_model.homework;
-//
-//public class Order {
-//    public Order(double value, String date, String userLogin) {
-//        this.value = value;
-//        this.date = date;
-//        this.userLogin = userLogin;
+package com.kodilla.execution_model.homework;
+
+import java.util.Objects;
+
+public class Order {
+    private double value;
+    private String date;
+    private String userLogin;
+
+    public Order(double value, String date, String userLogin) {
+        this.value = value;
+        this.date = date;
+        this.userLogin = userLogin;
+    }
+    public double getValue(){
+        return value;
+    }
+    public String getUserLogin(){
+        return userLogin;
+    }
+    public String getDate(){
+        return date;
+    }
+//    @Override
+//    public boolean equals(Objects o){
+//        if (this == o) return true;
+//        if (o == null || getClass() !=o.getClass()) return false;
+//        Order order = (Order) o;
+//        return Double.compare (value, order.value) == o && Objects.equals(date, order.date) && Objects.equals(userLogin, order.userLogin);
 //    }
-//}
+    @Override
+    public int hashCode(){
+        return Objects.hash(value,date, userLogin);
+    }
+}
--- kodilla-advanced-tests/src/test/java/com/kodilla/execution_model/homework/ShopTestSuite.java
+++ kodilla-advanced-tests/src/test/java/com/kodilla/execution_model/homework/ShopTestSuite.java
@@ -1,68 +1,48 @@
-//package com.kodilla.execution_model.homework;
-//
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.Test;
-//
-//import java.util.Set;
-//
-//import static org.junit.jupiter.api.Assertions.assertEquals;
-//import static org.junit.jupiter.api.Assertions.assertTrue;
-//
-//public class ShopTestSuite {
-//    private Shop shop;
-//    private Order order1, order2, order3;
-//
-//    @BeforeEach
-//    public void initializeOrder() {
-//        shop = new Shop();
-//        order1 = new Order(100.0, "2025-01-06", "user1");
-//        order2 = new Order(200.0, "2025-01-05", "user2");
-//        order3 = new Order(300.0, "2025-01-02", "user3");
-//        shop.addOrder(order1);
-//        shop.addOrder(order2);
-//        shop.addOrder(order3);
-//    }
-//
-//    @Test
-//    public void shouldAddNewOrder() {
-//        // given
-//        Order newOrder = new Order(150.0, "2025-01-03", "user4");
-//
-//        // when
-//        shop.addOrder(newOrder);
-//
-//        // then
-//
-//    }
-//
-//    @Test
-//    public void shouldReturnOrdersWithinDateRange() {
-//        // when
-//
-//
-//        // then
-//
-//    }
-//
-//    @Test
-//    public void shouldReturnOrdersWithinValueRange() {
-//        // when
-//
-//
-//        // then
-//
-//    }
-//
-//    @Test
-//    public void shouldReturnCorrectNumberOfOrders() {
-//        // then
-//
-//    }
-//
-//    @Test
-//    public void shouldSumOrderValues() {
-//        // then
-//
-//    }
-//}
-//
+package com.kodilla.execution_model.homework;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class ShopTestSuite {
+    private Shop shop;
+    private Order order1, order2, order3;
+
+    @BeforeEach
+    public void initializeOrder() {
+        shop = new Shop();
+        order1 = new Order(20.0, "2025-01-06", "user1");
+        order2 = new Order(160.0, "2025-01-05", "user2");
+        order3 = new Order(1440.0, "2025-01-02", "user3");
+        shop.addOrder(order1);
+        shop.addOrder(order2);
+        shop.addOrder(order3);
+    }
+    @Test
+    public void AddNewOrder(){
+        //given
+        Order newOrder = new Order(200, "2025-01-08", "user4");
+        //when
+        shop.addOrder(newOrder);
+
+        //then
+        assertEquals(4, shop.getNumberOfOrders());
+    }
+    @Test
+    public void ReturnOrderWithDataRange(){
+        //when
+        Set<Order> ordersInValueRange = shop.getOrdersInValueRange(150.0, 250.0);
+        //then
+        assertEquals(1, ordersInValueRange.size());
+        assertTrue(ordersInValueRange.contains(order2));
+    }
+    @Test
+    public void SumOderValues(){
+        assertEquals(1620.0, shop.sumOfOrderValues());
+    }
+}
+
